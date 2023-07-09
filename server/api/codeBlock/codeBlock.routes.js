9bc11f4126d367fb741f0cc3f72d728fdf190bfb
Shahar Saadon <shaharsaadon1@gmail.com>
2023-07-09 10:29:40 +0300

    general code improved

--- server/api/codeBlock/codeBlock.routes.js
+++ server/api/codeBlock/codeBlock.routes.js
@@ -6,8 +6,8 @@ const {
 } = require('./codeBlock.controller.js');
 const router = express.Router();
 
-router.get('/:id', getCodeBlockById);
 router.get('/', getCodeBlocks);
+router.get('/:id', getCodeBlockById);
 router.put('/:id', updateCodeBlock);
 
 module.exports = router;
